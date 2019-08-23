e18685444b98c30dbc1dadf50ff15c0b8e55d58f
Naveen <>
2019-08-23 12:17:52 +0530

    Added Routing in app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.js'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import App from './App.js';
+import Api from './Api';
+import About from './About.js';
 import './index.css';
 
-ReactDOM.render(<App />,document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <Route exact path="/" component={App} />
+            <Route exact path="/api" component={Api} />
+            <Route exact path="/about" component={About} />
+        </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
